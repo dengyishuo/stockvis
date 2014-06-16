65a5d7aa60c844adaf1dbf42b95c8c091598a442
hynial <hynials@gmail.com>
2014-06-16 13:20:37 +0800

    Create server.R
    server

---
+++ server.R
@@ -0,0 +1,24 @@
+# server.R
+
+library(quantmod)
+source("helpers.R")
+
+shinyServer(function(input, output) {
+  dataInput <- reactive({
+    getSymbols(input$symb,src = "yahoo",
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+  fileInput <- reactive({
+    if(!input$adjust) return (dataInput())
+    adjust(dataInput())
+  })
+  output$plot <- renderPlot({
+    
+    if(fileInput()) data <- adjust(dataInput())
+    chartSeries(dataInput(), theme = chartTheme("white"), 
+      type = "line", log.scale = input$log, TA = NULL)
+  })
+  
+})
